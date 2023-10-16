b42769eadd591120dae7bedf7ceabce1f883519e
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-16 05:27:31 -0700

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,12 @@ char letter = 'a';
 
 while (letter <= 'z')
 {
-if (letter != 'q' || letter != 'e')
+if (letter == 'q' || letter == 'e')
 {
-putchar(letter);
 letter++;
 }
+putchar(letter);
+letter++;
 }
 putchar('\n');
 return (0);
