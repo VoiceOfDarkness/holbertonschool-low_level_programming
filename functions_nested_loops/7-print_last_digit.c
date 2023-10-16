75c44984577bf01939a5daea238d004ea08cb751
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-16 09:19:51 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if char is lowercase
+ *
+ * @c: character to be checked if lower case, type int
+ *
+ * Return: 1 if lower case, 0 otherwise
+ **/
+
+int print_last_digit(int n)
+{
+int last_digit = n % 10;
+
+_puchar(last_digit + 48);
+}
