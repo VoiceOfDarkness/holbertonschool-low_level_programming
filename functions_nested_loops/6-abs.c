e6459a1ea56f8f1ac773ec0f7dc4bf22e8f2849f
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-16 09:14:52 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - returns 1 if char is lowercase
+ *
+ * @n: character changed to abs, type int
+ *
+ * Return: abs of @n
+ **/
+int _abs(int n)
+{
+if (n < 0)
+return (-n)
+else
+return (n)
+}
