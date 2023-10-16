ac0f18802e04e0b0e0a5aa06ea1f3eb70cf0e2d6
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-16 04:51:44 -0700

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+
+putchar('\n');
+return (0);
+}
