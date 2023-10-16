467aa28ad0a3ac89ca5d5a153bc06b62798f8341
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-16 08:52:14 -0700

    print_sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - returns 1 if char is lowercase
+ *
+ * @c: character to be checked if grater than zero etc, type int
+ *
+ * Return: 1 and prints + if > 0, 0 otherwise
+ **/
+int print_sign(int n)
+{
+if (n > 0)
+_putchar('+');
+return (1);
+else if (n == 0)
+_putchar(0 + 48);
+return (0);
+else
+_putchar('-');
+return (-1);
+}
