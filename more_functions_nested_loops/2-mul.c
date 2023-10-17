775a54ff895f09b715a9af896f9d9d66b1ff987f
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 07:04:19 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - returns 1 if char is lowercase
+ *
+ * @a: character to be checked if grater than zero etc, type int
+ *
+ * @b: character to be checked if g
+ *
+ * Return: 1 and prints + if > 0, 0 otherwise
+ **/
+int mul(int a, int b)
+{
+return (a * b);
+}
