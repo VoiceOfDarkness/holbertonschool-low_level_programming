071f424906a5ec6b243eb4b2126a1f68c1c0bbf8
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 07:37:15 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - returns 1 if char is lowercase
+ *
+ *
+ * Return: from 0 to 14
+ **/
+void more_numbers(void)
+{
+int i;
+int j;
+
+for (j = 0; j <= 9; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+{
+putchar((i / 10) + '0');
+}
+putchar(i % 10 + '0');
+}
+putchar('\n');
+}
+
+return 0;
+}
