05d12ce05151a70acada8e0f869ac865a04ecd8e
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 03:36:23 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - returns 1 if char is lowercase
+ *
+ * @a: character to be checked if grater than zero etc, type int
+ *
+ * @b: character to be checked if g
+ *
+ * Return: 1 and prints + if > 0, 0 otherwise
+ **/
+int add(int a, int b)
+{
+return (a + b);
+}
