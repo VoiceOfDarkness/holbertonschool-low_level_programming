f845bb5b42354b31ebc8c7cfed547a4af7fdd3fb
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 08:36:18 -0700

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,28 @@
 #include "main.h"
 /**
- * add - returns 1 if char is lowercase
- *
- * @a: character to be checked if grater than zero etc, type int
- *
- * @b: character to be checked if g
- *
- * Return: 1 and prints + if > 0, 0 otherwise
- **/
+* print_square - returns 1 if char is lowercase
+*
+* @size: character to be checked if grater than zero etc, type int
+*
+* Return: 1 and prints + if > 0, 0 otherwise
+**/
 void print_square(int size)
 {
-if (size < 0)
+int i;
+int j;
+if (size > 0)
 {
-_putchar('\n');
-}
-else
+for (i = 0; i < size; i++)
 {
-for (int i = 0; i < size; i++)
-{
-for (int j = 0; j < size; j++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
-
 _putchar('\n');
 }
+}
+else
+{
+_putchar('\n');
 }
 }
