1c30384d567579cea99d1e528491a511819bc57c
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 04:21:01 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,21 +12,35 @@ int n = -10;
 int border = 98;
 
 if (n < border)
+{
 while (n <= 98)
 {
 if (n == 98)
-printf("%d", n);
-else
-printf("%d, ", n);
-n++;
+{
+    printf("%d", n);
 }
 else
+{
+    printf("%d, ", n);
+}
+n++;
+}
+}
+else
+{
 while (n >= 98)
 {
 if (n == 98)
-printf("%d", n);
+{
+    printf("%d", n);
+}
 else
-printf("%d, ", n);
+{
+    printf("%d, ", n);
+}
 n--;
 }
 }
+}
+
+
