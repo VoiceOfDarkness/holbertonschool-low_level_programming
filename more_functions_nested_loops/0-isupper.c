d8d5df76d44431ba91d73201768468d2bdb986ee
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 06:50:33 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if char is lowercase
+ *
+ * @c: character to be checked if lower case, type int
+ *
+ * Return: 1 if lower case, 0 otherwise
+ **/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char);
+int _isupper(int c);
