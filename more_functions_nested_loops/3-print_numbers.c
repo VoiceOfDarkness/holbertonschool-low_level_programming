bf5693caf7f4cefe1766d7419d5fa40b84d66579
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 07:09:14 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - returns 1 if char is lowercase
+ *
+ *
+ * Return: from 0 to 9
+ **/
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + 48);
+}
+_putchar('\n');
+}
