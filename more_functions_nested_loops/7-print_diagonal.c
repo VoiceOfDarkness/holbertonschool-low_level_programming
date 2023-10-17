b30fa881fe247fcd2b44c1837cd116711988c1b4
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-17 08:02:04 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - returns 1 if char is lowercase
+ *
+ * @n: character to be checked if grater than zero etc, type int
+ *
+ * Return: 1 and prints + if > 0, 0 otherwise
+ **/
+void print_diagonal(int n)
+{
+int i;
+int j;
+
+if (n > 0)
+{
+for (j = 0; j <= 9; j++)
+{
+for (i = 0; i < j - 1; i++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
