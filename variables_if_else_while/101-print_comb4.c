88378779e42667420f6cd324300db0f0a2de5d72
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-18 04:23:02 -0700

    101-print_comb4.c

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j, k;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+for (k = 0; k <= 9; k++)
+{
+if (i < j && j < k)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i < 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+
+return (0);
+}
