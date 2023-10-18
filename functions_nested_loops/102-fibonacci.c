07c47e16ab14b4832da5a2069b04af8b82036043
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-18 05:59:01 -0700

    102-fibonacci.c

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -5,8 +5,8 @@
  * Return: Always 0 (Success)
  */
 int main() {
-int n = 50, i;
-unsigned long fib[50];
+int n = 52, i;
+unsigned long long fib[52];
 
 fib[0] = 0;
 fib[1] = 1;
@@ -15,9 +15,17 @@ for (i = 2; i < n; i++) {
 fib[i] = fib[i-1] + fib[i-2];
 }
 
-for (i = 0; i < n; i++) {
-printf("%lu ", fib[i]);
+for (i = 2; i < n; i++) {
+if (i < 51)
+{
+printf("%llu, ", fib[i]);
+}
+else
+{
+printf("%llu", fib[i]);
+}
 }
 
-return 0;
+printf("\n");
+return (0);
 }
