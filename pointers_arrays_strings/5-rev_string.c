34a404176c3b54d470e8f99b4d9e924068a010fc
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-23 05:29:10 -0700

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,16 +11,16 @@ void rev_string(char *s)
 int len = 0;
 int i = 0;
 
-while (str[len] != '\0')
+while (s[len] != '\0')
 len++;
 
 for (i = 0; i <= len; i++)
-_putchar(str[i]);
+_putchar(s[i]);
 
 _putchar('\n');
 
 for (len = len - 1; len >= 0; len--)
-_putchar(str[len]);
+_putchar(s[len]);
 
 _putchar('\n');
 }
