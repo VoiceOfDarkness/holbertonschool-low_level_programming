c5be026f5380aa960ce0e13fc28ec39ac39fb499
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-23 06:25:35 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - Entry point
+ *
+ *@dest: string
+ *
+ *@src: string
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i] != '\0')
+dest[i] = src[i];
+
+dest[i] = '\0';
+return (dest);
+}
