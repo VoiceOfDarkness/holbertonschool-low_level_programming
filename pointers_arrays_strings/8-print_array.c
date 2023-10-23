54db72911d3b74995ab5a35a8c81d3e3a5705157
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-23 06:12:28 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - Entry point
+ *
+ *@a: array
+ *
+ *@n: length or array
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+for (int i = 0; i < n; i++)
+if (i < n - 1)
+printf("%d, ", a[i]);
+else
+printf("%d", a[i]);
+}
