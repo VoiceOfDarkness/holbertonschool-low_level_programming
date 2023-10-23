09cb9eb2448d78d072b63fa3665030c2472e1040
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-23 04:58:25 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+/**
+ * print_rev - Entry point
+ *
+ *@s: string
+ *
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+i++;
+
+for (i = i - 1; i >= 0; i--)
+putchar(s[i]);
+
+putchar('\n');
+}
+
