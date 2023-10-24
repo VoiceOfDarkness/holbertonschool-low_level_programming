bce85dbccf9a8df020994adbe68cd2806de817ad
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-24 07:57:45 -0700

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - Concatenate two strings, limiting the number of characters copied
+ *
+ * @dest: Destination string
+ * @src: Maximum number of characters to copy from src
+ * @n: unsigned int
+ *
+ * Return: Pointer to the concatenated string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+while (n--)
+{
+*dest++ = *src++;
+}
+return (dest);
+}
+
