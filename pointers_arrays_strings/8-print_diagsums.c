ace285ba8e5284f7897025a168c285abecbcee88
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-24 08:56:16 -0700

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagsums - Concatenate two strings, limiting
+ *
+ * @a: Destination string
+ * @size: int
+ *
+ * Return: Pointer to the concatenated string
+ */
+void print_diagsums(int *a, int size)
+{
+int i;
+int sum1 = 0, sum2 = 0;
+
+for (i = 0; i < size; i++)
+{
+sum1 += *(a + i * size + i);
+sum2 += *(a + i * size + (size - 1 - i));
+}
+printf("%d, %d\n", sum1, sum2);
+}
