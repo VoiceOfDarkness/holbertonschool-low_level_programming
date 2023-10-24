da9eebdc2abc5b1f715b74b75ee161339f0a8cb1
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-24 08:04:38 -0700

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - Concatenate two strings, limiting the number of characters copied
+ *
+ * @s: Destination string
+ * @c: Maximum number of characters to copy from src
+ *
+ * Return: Pointer to the concatenated string
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+	return (&s[i]);
+}
+}
+return (NULL);
+}
