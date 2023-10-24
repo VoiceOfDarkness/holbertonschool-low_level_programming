b507173e0a72cdf7a7b3cf6879ff59abee6bfede
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-24 05:06:44 -0700

    2-str

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncpy - Entry point
+ *
+ *@dest: string
+ *
+ *@src: string
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    int len;
+
+    for (len = 0; len < n && *(src + len); len++)
+    {
+        *(dest + len) = *(src + len);
+    }
+    for (; len < n; len++)
+    {
+        *(dest + len) = '\0';
+    }
+    return (dest);
+}
