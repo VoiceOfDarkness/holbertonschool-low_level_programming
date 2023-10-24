de9d8db08b85479daabde66529aded98ab0dba4d
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-24 05:07:48 -0700

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int len;
+int len;
 
-    for (len = 0; len < n && *(src + len); len++)
-    {
-        *(dest + len) = *(src + len);
-    }
-    for (; len < n; len++)
-    {
-        *(dest + len) = '\0';
-    }
-    return (dest);
+for (len = 0; len < n && *(src + len); len++)
+{
+*(dest + len) = *(src + len);
+}
+for (; len < n; len++)
+{
+*(dest + len) = '\0';
+}
+return (dest);
 }
