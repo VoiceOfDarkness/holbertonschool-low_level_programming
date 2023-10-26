4d2982ab9aa23b0926f6046d30fc1d956965323b
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-26 06:13:17 -0700

    100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -2,12 +2,12 @@
 /**
 * rot13 - Encodes a string using the rot13 algorithm
 *
-* This function takes a string as input and applies the rot13 
-* algorithm to it, which replaces each letter with the letter 13 
-* positions ahead of it in the alphabet. The function initializes 
-* two arrays of characters, a and b, which contain the letters of 
-* the alphabet in order and the corresponding letters 13 positions ahead. 
-* The function then iterates over the input string, replacing each letter 
+* This function takes a string as input and applies the rot13
+* algorithm to it, which replaces each letter with the letter 13
+* positions ahead of it in the alphabet. The function initializes
+* two arrays of characters, a and b, which contain the letters of
+* the alphabet in order and the corresponding letters 13 positions ahead.
+* The function then iterates over the input string, replacing each letter
 * with the corresponding letter from b if it is found in a. The function returns the modified string.
 *
 * @str: The string to be encoded
@@ -20,7 +20,7 @@ int i, j;
 char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-for (i = 0; i < 100; i++)
+for (i = 0; *(str + i); i++)
 {
 for (j = 0; j < 52; j++)
 {
