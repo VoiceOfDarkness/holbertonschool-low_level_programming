98d8db097c24107d3ccd8307121cec8a4a7f6457
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-27 07:56:24 -0700

    103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
-* _strlen - Calculate the length of a string.
-*
-* @str: A pointer to the string whose length is to be calculated.
-*
-* Return: An integer representing the length of the input string.
-*/
+ * _strlen - Calculate the length of a string.
+ *
+ * @str: A pointer to the string whose length is to be calculated.
+ *
+ * Return: An integer representing the length of the input string.
+ */
 int _strlen(char *str)
 {
 int length = 0;
 while (*str != '\0')
 {
-length++;
-str++;
+	length++;
+	str++;
 }
 return (length);
 }
 
 /**
-* infinite_add - Add two numbers represented as strings.
-*
-* @a: A pointer to the first number
-* @b: A pointer to the second numbe
-* @result: A pointer to the destinat
-* @size_result: An integer
-*
-* Return: This function returns the `result`
-*/
+ * infinite_add - Add two numbers represented as strings.
+ *
+ * @a: A pointer to the first number
+ * @b: A pointer to the second numbe
+ * @result: A pointer to the destinat
+ * @size_result: An integer
+ *
+ * Return: This function returns the `result`
+ */
 char *infinite_add(char *a, char *b, char *result, int size_result)
 {
 int i, j, k, carry, sum, len_a, len_b;
@@ -41,30 +41,30 @@ k = 0;
 
 for (i = len_a - 1, j = len_b - 1; i >= 0 || j >= 0; i--, j--, k++)
 {
-sum = carry;
-if (i >= 0)
-sum += a[i] - '0';
-if (j >= 0)
-sum += b[j] - '0';
-result[k] = (sum % 10) + '0';
-carry = sum / 10;
+	sum = carry;
+	if (i >= 0)
+		sum += a[i] - '0';
+	if (j >= 0)
+		sum += b[j] - '0';
+	result[k] = (sum % 10) + '0';
+	carry = sum / 10;
 }
 
 if (k >= size_result - 1)
 {
-result[k] = '\0';
-return (0);
+	result[k] = '\0';
+	return (0);
 }
 
 if (carry)
-result[k++] = carry + '0';
+	result[k++] = carry + '0';
 result[k] = '\0';
 
 for (i = 0, j = k - 1; i < j; i++, j--)
 {
-temp = result[i];
-result[i] = result[j];
-result[j] = temp;
+	temp = result[i];
+	result[i] = result[j];
+	result[j] = temp;
 }
 return (result);
 }
