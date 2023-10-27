b6258130d836c857b907d14c3e682ab14cac411b
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-27 08:19:34 -0700

    100-set_string.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -30,4 +30,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void set_string(char **s, char *to)
 #endif /* MAIN_H */
