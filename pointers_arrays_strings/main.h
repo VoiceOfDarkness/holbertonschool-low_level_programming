072ed9dd29db87409615dea238f0a13c3960fca1
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-27 08:09:20 -0700

    103-infinite_add.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_number(int n);
 void print_buffer(char *b, int size)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
