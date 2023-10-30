9d718c75f4a690a0a0f17c005cc12cb9f341cf91
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-30 05:27:15 -0700

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - Entry point
+ *
+ *@n: int
+ *
+ * Return: Always 0 (Success)
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+
+if (n <= 1)
+{
+return (1);
+}
+
+return factorial(n - 1) * n;
+}
