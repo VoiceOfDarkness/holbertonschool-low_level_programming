3958c780abb194208329ff023e5f47d112d299e4
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-31 06:28:16 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print name of file
+ * Return: zero
+ **/
+int main(int argc, char **argv)
+{
+(void)argv;
+
+printf("%d\n", argc - 1);
+return (0);
+}
