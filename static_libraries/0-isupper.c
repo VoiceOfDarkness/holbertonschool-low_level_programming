83ee2dea08508ac02e448b92f2a54d17cd5258a8
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-31 04:45:19 -0700

    update

--- static_libraries/0-isupper.c
+++ static_libraries/0-isupper.c
@@ -1,3 +1,16 @@
 #include "main.h"
-
-int _isupper(int c);
+/**
+ * _isupper - returns 1 if char is lowercase
+ *
+ * @c: character to be checked if upper case, type int
+ *
+ * Return: 1 if lower case, 0 otherwise
+ **/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
