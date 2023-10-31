3c442525fd00dbae523076fded4fa0541e380134
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-31 06:31:10 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print name of file
+ * Return: zero
+ **/
+int main(int argc, char **argv)
+{
+
+int i;
+(void)argv;
+
+for (i = 0; i < argc; i++)
+{
+printf("%d\n", i);
+}
+}
