c172b0d8c84c2dc4d15513590e7be1052101a474
Abilhasan Samadov <7418@holbertonstudents.com>
2023-10-31 06:36:31 -0700

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print name of file
+ * Return: zero
+ **/
+int main(int argc, char **argv)
+{
+int x, y;
+
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x * y);
+
+return (0);
+}
