ddb586170a52923d32867f72a9c31add466899f4
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-06 03:52:41 -0800

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _strdup - Entry point
+*
+* @str: char string
+*
+* Return: Always 0 (Success)
+*/
+char *_strdup(char *str)
+{
+char *result;
+int len;
+if (str == NULL)
+{
+return (NULL);
+}
+len = strlen(str);
+result = (char *)malloc(sizeof(char) * len);
+if (result == NULL)
+{
+return (NULL);
+}
+strcpy(result, str);
+return (result);
+}
