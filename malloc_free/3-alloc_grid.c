db27e2de1ec86b926f54b5810a7057af0636e98d
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-06 04:39:12 -0800

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* **aloc_grid - Entry point
+*
+* @width: char string
+* 
+* @height: char string
+*
+* Return: Always 0 (Success)
+*/
+int **alloc_grid(int width, int height)
+{
+int i, j;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+int **grid;
+
+grid = malloc(sizeof(int *) * height);
+
+if (grid == NULL)
+return (NULL);
+
+for (i = 0; i < height; i ++)
+{
+grid[i] = malloc(sizeof(int) * width);
+
+for (j = 0; j < width; j++)
+{
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+return (NULL);
+}
+}
+}
+
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+grid[i][j] = 0;
+}
+
+return (grid);
+}
