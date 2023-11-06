0445f63c0892a881968ffbe12c3f5002e66f2244
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-06 04:47:42 -0800

    4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* free_grid - Entry point
+*
+* @height: int variable
+*
+* Return: Always 0 (Success)
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+
+}
