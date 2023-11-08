1ababc30e0ef9bb7b4253283b257263d0cbb9f34
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-08 00:59:02 -0800

    hot

--- preprocessor/1-pi.h
+++ preprocessor/1-pi.h
@@ -1,2 +1,6 @@
-#undef PI
+#ifndef _PI_H_
+#define _PI_H_
+
 #define PI 3.14159265359
+
+#endif
--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,2 +1,6 @@
-#undef SUM
+#ifndef _SUM_H_
+#define _SUM_H_
+
 #define SUM(x, y) ((x) + (y))
+
+#endif
