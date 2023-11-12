57e5d950da2e30e96ccf22d464164af198ba0bf0
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-12 02:19:11 -0800

    101-mul

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,73 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>print_error_and_exit
 /**
-* is_digit - checks if a string is composed only of digits
-* @str: the string to check
-*
-* Return: 1 if the string is composed only of digits, 0 otherwise
-*/
-int is_digit(char *str)
-{
-while (*str)
-{
-if (*str < '0' || *str > '9')
-return (0);
-str++;
+ * print_error_and_exit - print error and exit
+ * Return: void
+ */
+void print_error_and_exit(void) {
+    printf("Error\n");
+    exit(98);
 }
-return (1);
-}
-
 /**
-* multiply - multiplies two numbers
-* @num1: the first number
-* @num2: the second number
-*
-* Return: the result of the multiplication
-*/
-int multiply(int num1, int num2)
-{
-return (num1 * num2);
+ * is_number - checkin is number
+ * @str: string
+ *
+ * Return: 1
+ */
+int is_number(char *str) {
+    while (*str) {
+        if (!isdigit(*str))
+            return 0;
+        str++;
+    }
+    return 1;
 }
-
 /**
-* main - entry point
-* @argc: the number of arguments
-* @argv: the arguments
-*
-* Return: 0 on success, 98 on failure
-*/
-int main(int argc, char **argv)
-{
-int num1, num2, *result_ptr;
-char *error_message = "Error\n";
+ * main - multiplies two numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[]) {
+    if (argc != 3)
+        print_error_and_exit();
 
-if (argc != 3)
-{
-printf("%s", error_message);
-return (98);
-}
+    if (!is_number(argv[1]) || !is_number(argv[2]))
+        print_error_and_exit();
 
-if (!is_digit(argv[1]) || !is_digit(argv[2]))
-{
-printf("%s", error_message);
-return (98);
-}
+    unsigned long num1 = strtoul(argv[1], NULL, 10);
+    unsigned long num2 = strtoul(argv[2], NULL, 10);
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
+    unsigned long result = num1 * num2;
 
-result_ptr = (int *)malloc(sizeof(int));
-if (result_ptr == NULL) {
-printf("%s", error_message);
-return (98);
-}
+    printf("%lu\n", result);
 
-*result_ptr = multiply(num1, num2);
-
-printf("%d\n", *result_ptr);
-
-free(result_ptr);
-
-return (0);
+    return 0;
 }
