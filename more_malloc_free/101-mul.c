6f15e71c6df83e2ff62d107906dffcb32bd0dabf
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-12 02:22:23 -0800

    101-mul

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,48 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>print_error_and_exit
+#include <ctype.h>
 /**
- * print_error_and_exit - print error and exit
- * Return: void
- */
+* print_error_and_exit - print error and exit
+* Return: void
+*/
 void print_error_and_exit(void) {
-    printf("Error\n");
-    exit(98);
+printf("Error\n");
+exit(98);
 }
 /**
- * is_number - checkin is number
- * @str: string
- *
- * Return: 1
- */
+* is_number - checkin is number
+* @str: string
+*
+* Return: 1
+*/
 int is_number(char *str) {
-    while (*str) {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
-    return 1;
+while (*str) {
+if (!isdigit(*str))
+return (0);
+str++;
+}
+return (1);
 }
 /**
- * main - multiplies two numbers.
- * @argc: argument count
- * @argv: arguments
- *
- * Return: 0
- */
+* main - multiplies two numbers.
+* @argc: argument count
+* @argv: arguments
+*
+* Return: 0
+*/
 int main(int argc, char *argv[]) {
-    if (argc != 3)
-        print_error_and_exit();
+unsigned long num1, num2, result;
 
-    if (!is_number(argv[1]) || !is_number(argv[2]))
-        print_error_and_exit();
+if (argc != 3)
+print_error_and_exit();
 
-    unsigned long num1 = strtoul(argv[1], NULL, 10);
-    unsigned long num2 = strtoul(argv[2], NULL, 10);
+if (!is_number(argv[1]) || !is_number(argv[2]))
+print_error_and_exit();
 
-    unsigned long result = num1 * num2;
+num1 = strtoul(argv[1], NULL, 10);
+num2 = strtoul(argv[2], NULL, 10);
 
-    printf("%lu\n", result);
+result = num1 * num2;
 
-    return 0;
+printf("%lu\n", result);
+
+return (0);
 }
