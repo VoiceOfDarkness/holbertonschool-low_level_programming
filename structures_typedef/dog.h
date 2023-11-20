65c6d8a6c24c80e3319a3566d41792ae4f80979e
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-20 04:27:04 -0800

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ * @name: Name of the dog
+ * @age: Age of the age
+ * @owner: Owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
