5c111492f37a873b07a222e03f271fc57dadd9d1
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-20 05:55:25 -0800

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog
+ * @d: dog to be freed
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return;
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
