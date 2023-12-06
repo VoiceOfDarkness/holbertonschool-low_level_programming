1e19da08bdb3ff354054560ba1260ae97554170f
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-06 11:24:10 -0800

    0-binary_to_uint.c

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; i < '\0'; i++)
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
+	}
+
+	for (i = 0; i < '\0'; i++)
+	{
+		result <<= 1;
+
+		if (b[i] == '1')
+			result += 1;
+	}
+
+	return (result);
+}
