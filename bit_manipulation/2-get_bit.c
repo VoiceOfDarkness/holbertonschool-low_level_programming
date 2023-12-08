4ecd670989cb3d2c848d4458a89d638e8ce14031
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-08 03:22:28 -0800

    hot fix

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,8 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
 	for(; index > 0; index--)
 		n >>= 1;
 
-	return (n);
+	return (n & 1);
 }
