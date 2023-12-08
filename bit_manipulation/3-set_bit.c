7d193a557633da7ad77d0d4221b9c112fa0b26e9
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-08 03:50:25 -0800

    3-set_bit.c

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int temp;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	temp = 1 << index;
+	*n = *n | temp
+
+	return (1);
+}
