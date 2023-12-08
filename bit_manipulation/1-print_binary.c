0b168ca63244a4e6388706971d1375c0bcc67107
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-08 02:54:28 -0800

    1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -2,9 +2,13 @@
 
 void print_binary(unsigned long int n)
 {
+	unsigned long int result;
+
 	if (n > 1)
 		print_binary(n >> 1);
-	printf("%lu", n & 1);
+
+	result = n & 1;
+	putchar(result + '0');
 
 	return;
 }
