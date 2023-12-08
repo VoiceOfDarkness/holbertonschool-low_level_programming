7fea3a8ad961314dd675775c5002e298adf28c64
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-08 04:49:45 -0800

    hot fix

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to check
+ * @index: index to check
+ *
+ * Return: value of bit at index or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	for (; index > 0; index--)
+		n >>= 1;
+
+	return (n & 1);
+}
+
+/**
  * flip_bits - returns the number of bits you would need to flip to get from
  *
  * @n: number to flip
