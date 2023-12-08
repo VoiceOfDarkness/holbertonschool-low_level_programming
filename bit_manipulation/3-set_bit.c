8f09e5f0e4a473eb3ac7db74f629a72018ec631f
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-08 03:51:04 -0800

    hot fix

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	temp = 1 << index;
-	*n = *n | temp
+	*n = *n | temp;
 
 	return (1);
 }
