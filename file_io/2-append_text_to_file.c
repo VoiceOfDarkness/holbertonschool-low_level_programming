f1fbaa202795659361af1ac6302a66e25a366085
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-11 06:58:19 -0800

    hot fix

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (text_content != NULL)
-		len = write(fd, text_content, strlen(text_content);
+		len = write(fd, text_content, strlen(text_content));
 
 	close(fd);
 
