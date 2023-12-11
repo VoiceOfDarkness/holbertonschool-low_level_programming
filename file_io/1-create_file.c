85c82a9c70e1448b58a114dbfe5af63fae124c78
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-11 06:40:01 -0800

    hot fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename,  O_CRATE | O_RDWR | O_TRUNC, 0600);
+	o = open(filename,  O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (w == -1 || o == -1)
