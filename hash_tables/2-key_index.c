5b2c47e26cd4dadbffe50354ae1515b55b86db39
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-13 06:36:47 -0800

    hot fix

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - check the code for
+ *
+ * @key: key
+ * @size: size of the array
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
