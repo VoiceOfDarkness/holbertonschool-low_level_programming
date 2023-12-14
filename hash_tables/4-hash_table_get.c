8ab4b76f41cf5c2145c3373aa09a0316fe897743
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-14 06:09:33 -0800

    4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t temp;
+	unsigned long int idx;
+
+	if (key == NULL)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	temp = ht->array[idx];
+
+	while (temp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
