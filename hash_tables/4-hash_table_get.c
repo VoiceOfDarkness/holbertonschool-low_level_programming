23fbfa9be26ddac8363879c7d649762af672d3f5
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-14 06:13:33 -0800

    hot fix

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,11 +1,18 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to look for
+ *
+ * Return: value associated with the element, or NULL if key couldn't be found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *temp;
 	unsigned long int idx;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL)
 		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
