c06de8acd294e2bc82794984559dcd88cc5e85f2
Abilhasan Samadov <7418@holbertonstudents.com>
2023-12-14 10:29:29 -0800

    hot fix

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,7 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	char *comma = "";
 
-	if (ht == NULL && ht->array == NULL)
+	if (ht == NULL || ht->array == NULL)
 		return;
 
 	printf("{");
